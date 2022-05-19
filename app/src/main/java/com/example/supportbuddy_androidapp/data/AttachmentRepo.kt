618ecf8083f46e0c74227716390bbec7a5c49c08
madsq98-desktop <mads.q@outlook.com>
2022-05-19 16:04:01 +0200

    Update AttachmentRepo.kt

--- app/src/main/java/com/example/supportbuddy_androidapp/data/AttachmentRepo.kt
+++ app/src/main/java/com/example/supportbuddy_androidapp/data/AttachmentRepo.kt
@@ -9,11 +9,20 @@ import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
 import cz.msebera.android.httpclient.Header
+import cz.msebera.android.httpclient.HttpResponse
+import cz.msebera.android.httpclient.client.HttpClient
+import cz.msebera.android.httpclient.client.methods.HttpPost
+import cz.msebera.android.httpclient.entity.mime.MultipartEntityBuilder
+import cz.msebera.android.httpclient.entity.mime.content.FileBody
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient
+import cz.msebera.android.httpclient.message.BasicNameValuePair
 import org.json.JSONArray
 import org.json.JSONObject
 import org.json.JSONTokener
+import java.io.BufferedReader
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.InputStreamReader
 
 class AttachmentRepo constructor(private val context: Context) {
 
@@ -23,7 +32,41 @@ class AttachmentRepo constructor(private val context: Context) {
     fun addAttachment(attachmentUrl: String) : AttachmentDTO_Out{
         var attachmentDtoOut = AttachmentDTO_Out(0,"")
 
+        val customHttpClient : HttpClient = DefaultHttpClient()
+        val customHttpPost : HttpPost = HttpPost(url)
+
         val attachmentFile = File(attachmentUrl)
+
+        val nameValuePairs : MutableList<BasicNameValuePair> = ArrayList<BasicNameValuePair>()
+        nameValuePairs.add(BasicNameValuePair("filename", attachmentFile.name))
+
+        customHttpClient.params.setParameter("Connection","Keep-Alive")
+        customHttpClient.params.setParameter("Content-Type", "multipart/form-data")
+
+        val entity : MultipartEntityBuilder = MultipartEntityBuilder.create();
+        for(pair in nameValuePairs) {
+            entity.addTextBody(pair.name, pair.value)
+        }
+
+        entity.addPart("file", FileBody(attachmentFile))
+
+        customHttpPost.entity = entity.build()
+
+        val response : HttpResponse = customHttpClient.execute(customHttpPost)
+
+        val reader : BufferedReader = BufferedReader(InputStreamReader(response.entity.content, "UTF-8"))
+        val jsonResponse = reader.readLine()
+
+        val parsedJson = JSONTokener(jsonResponse).nextValue() as JSONObject
+
+        val attachmentId = parsedJson.getInt("id")
+        val attachmentWebUrl = parsedJson.getString("url")
+
+        val newAttachment = AttachmentDTO_Out(attachmentId, attachmentWebUrl)
+
+        attachmentDtoOut = newAttachment
+
+        /*
         val params = RequestParams()
 
         try {
@@ -52,6 +95,7 @@ class AttachmentRepo constructor(private val context: Context) {
 
             }
         })
+         */
 
         return attachmentDtoOut
     }
